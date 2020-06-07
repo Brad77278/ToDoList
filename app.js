d05a47ad645ccdf1003aa0e2d689179d44ea8646
Bradley Gray <bradleygray888@yahoo.com>
2020-06-07 16:53:04 -0700

    .

--- app.js
+++ app.js
@@ -6,21 +6,30 @@ const ejs = require("ejs");
 
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/ToDoListDB", { useNewUrlParser: true, useUnifiedTopology: true });
-
-const ToDoListScehma = new mongoose.Schema({
-
-})
-
-var items = ["Buy food", "cook food", "eat food"];
-
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static(__dirname + '/public'));
-
 app.set('view engine', 'ejs');
 
+mongoose.connect("mongodb://localhost:27017/ToDoListDB", { useNewUrlParser: true, useUnifiedTopology: true });
+
+//mongodb schema 
+const itemsScehma = new mongoose.Schema({
+    name: String
+});
+
+const Item = mongoose.model("Item", itemsScehma);
+
+//default item to fill blank list
+const item1 = new Item({
+    name: "Eat breakfast"
+});
+
+const defaultItems = [item1];
+
+//Home route
 app.get("/", function (req, res) {
+
+    //displays current date
     var today = new Date();
 
     var options = {
@@ -29,17 +38,60 @@ app.get("/", function (req, res) {
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
+    //populate list. If empty it populates with defualt item
+    Item.find({}, function (err, foundItems) {
 
-    res.render("list", { kindOfDay: day, newListItems: items });
+        if (foundItems.length === 0) {
+
+            Item.insertMany(defaultItems, function (err) {
+
+                if (err) {
+                    console.log(err);
+
+                } else {
+                    console.log("Successfully saved default items to database");
+                }
+            });
+
+            res.redirect("/");
+
+        } else {
+
+            res.render("list", { kindOfDay: day, newListItems: foundItems });
+        }
+    });
+
+    var day = today.toLocaleDateString("en-US", options);
 });
 
+//post route once item has been added and submit button has been selected
 app.post("/", function (req, res) {
-    var item = req.body.newItem;
-    items.push(item);
+
+    const itemName = req.body.newItem;
+
+    const item = new Item({
+        name: itemName
+    });
+
+    item.save();
+
     res.redirect("/");
-})
+});
+
+//post route for when an item is deleted
+app.post("/delete", function (req, res) {
+
+    const checkedItemId = req.body.checkbox;
+
+    Item.findByIdAndRemove(checkedItemId, function (err) {
+        if (!err) {
+            console.log("Successfully deleted checked item");
+            res.redirect("/");
+        }
+    });
+});
 
 app.listen(3000, function () {
+
     console.log("Server is running on port 3000");
 });
